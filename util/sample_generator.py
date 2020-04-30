e6a8b8aaa2731dd1ed35281a41424e60ff88c30f
yutveg <kodi.0n31l@gmail.com>
2020-04-30 10:53:22 -0700

    bugs fixed now to save attributes as id on room drs

--- util/sample_generator.py
+++ util/sample_generator.py
@@ -1,51 +1,30 @@
-# Sample Python code that can be used to generate rooms in
-# a zig-zag pattern.
-#
-# You can modify generate_rooms() to create your own
-# procedural generation algorithm and use print_rooms()
-# to see the world.
-
-
+from adventure.models import Room
 import numpy as np
 
-class Room():
-    def __init__(self, title, description):
-        self.title = title
-        self.description = description
-        self.n_to = None
-        self.e_to = None
-        self.s_to = None
-        self.w_to = None
-    def __repr__(self):
-        if self.e_to is not None:
-            return f"({self.x}, {self.y}) -> ({self.e_to.x}, {self.e_to.y})"
-        return f"({self.x}, {self.y})"
-    def connect_rooms(self, connecting_room, direction):
-        '''
-        Connect two rooms in the given n/s/e/w direction
-        '''
-        reverse_dirs = {"n": "s", "s": "n", "e": "w", "w": "e"}
-        reverse_dir = reverse_dirs[direction]
-        setattr(self, f"{direction}_to", connecting_room)
-        setattr(connecting_room, f"{reverse_dir}_to", self)
-    def get_room_in_direction(self, direction):
-        '''
-        Connect two rooms in the given n/s/e/w direction
-        '''
-        return getattr(self, f"{direction}_to")
+# class Room():
+#     def __init__(self, title, description):
+#         self.title = title
+#         self.description = description
+#         self.n_to = None
+#         self.e_to = None
+#         self.s_to = None
+#         self.w_to = None
 
+Room.objects.all().delete()
 
-class World:
+class World():
     def __init__(self, size = 100):
         self.size = size
-        self.mid = int(size ** (1/2)) * 3 + 1
+        self.mid = int(size ** (1/2)) * 2 + 1
         diameter = self.mid * 2 - 1
         self.grid = [[' ' for col in range(diameter)] for row in range(diameter)]
     def generate_rooms(self):
+        rooms = [[None for col in range(self.mid * 2 - 1)] for row in range(self.mid * 2 - 1)]
         dirs = ['n_to', 'e_to', 's_to', 'w_to']
-        entrance = Room('entrance', 'description')
-#         entrance.save()
-        self.grid[self.mid][self.mid] = 'O'        
+        entrance = Room(title='entrance', description='description')
+        self.grid[self.mid][self.mid] = 'O'
+        rooms[self.mid][self.mid] = entrance
+        bonus = 0
         for i in range(self.size):
             flag = True
             room = entrance            
@@ -54,24 +33,38 @@ class World:
             while flag:
                 r = np.random.randint(4)
                 dx = dirs[r]
+                rdx = dirs[(r + 2) % 4]
                 row += 2 * (dx == 's_to') - 2 * (dx == 'n_to')
                 col += 2 * (dx == 'e_to') - 2 * (dx == 'w_to')
                 if self.grid[row][col] == ' ':
-                    n_room = Room('title', 'description')
-                    setattr(n_room, dirs[(r + 2) % 4], room)
-#                     n_room.save()
+                    n_room = Room(title='title', description='description')
+                    n_room.save()
+                    setattr(n_room, rdx, room)
                     setattr(room, dx, n_room)
                     self.grid[row][col] = 'X'
                     self.grid[row + (dx == 'n_to') - (dx == 's_to')][col + (dx == 'w_to') - (dx == 'e_to')] = '*'
+                    rooms[row][col] = n_room
                     flag = False
+                else:
+                    if np.random.randint(self.size) == 0:
+                        o_room = rooms[row][col]
+                        setattr(room, dx, o_room)
+                        setattr(o_room, rdx, room)
+                        self.grid[row + (dx == 'n_to') - (dx == 's_to')][col + (dx == 'w_to') - (dx == 'e_to')] = '*'
+                    room = rooms[row][col]
+        for _ in self.grid:
+            print(''.join(_))
+
+        for line in rooms:
+            for room in line:
+                if room != None:
+                    room.save()
 
     def print_rooms(self):   
         map = []             
         for _ in self.grid:
             map.append(''.join(_)) 
-
         return map
-
 
     
 
